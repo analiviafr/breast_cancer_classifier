4aef24543fe77f615b2faa1a8d42135b12fb6c46
Ana Lívia Franco <46753870+analiviafr@users.noreply.github.com>
2020-03-25 23:19:26 -0300

    Update cancerdemama_carregar.py

--- cancerdemama_carregar.py
+++ cancerdemama_carregar.py
@@ -1,6 +1,3 @@
-#CARREGAR REDE JÁ CRIADA
-#continuação cancerdemama_salvar
-
 import numpy as np
 import pandas as pd
 from keras.models import model_from_json
@@ -10,9 +7,7 @@ estrutura_rede = arquivo.read()
 arquivo.close()
 
 classificador = model_from_json(estrutura_rede)
-classificador.load_weights('classificador_breast.h5')
-
-#teste 
+classificador.load_weights('classificador_breast.h5') 
 novo = np.array([[15.80, 8.34, 118, 900, 0.10, 0.26, 0.08, 0.134, 0.178,
                   0.20, 0.05, 1098, 0.87, 4500, 145.2, 0.005, 0.04, 0.05, 0.015,
                   0.03, 0.007, 23.15, 16.64, 178.5, 2018, 0.14, 0.185,
